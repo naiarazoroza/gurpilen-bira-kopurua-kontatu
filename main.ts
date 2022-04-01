fa98556ef0ecb070ae1bd4b2326d75316575e856
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2022-04-01 14:26:40 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,8 @@ basic.forever(function () {
 })
 basic.forever(function () {
     birak = parseFloat(DFRobotMaqueenPlus.readeDistance(Motors1.M1))
-    if (birak < 6.2) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 60)
+    if (birak < 17) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
     } else {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
     }
